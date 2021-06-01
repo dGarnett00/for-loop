a41a9179df9eb9d07c8d1538f5c21767c8219459
bruh man 5th floor <davonta2222@gmail.com>
2021-06-01 06:48:29 -0700

    Add project files.

---
+++ for loop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace for_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            for (int i = 1; i <= 10; i++)
+            {   
+                Console.WriteLine(i);
+
+            }
+        }
+    }
+}
